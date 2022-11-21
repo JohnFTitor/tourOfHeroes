0b7792dcb4dcab0386f0418428254d2700bb0c62
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-11-21 11:34:14 -0500

    Make hero details a route view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,18 +11,12 @@ import { Hero } from './models';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = []
-  selectedHero?: Hero
 
-  constructor(private heroService: HeroService, private messageService: MessageService){ }
+  constructor(private heroService: HeroService){ }
 
   ngOnInit(): void {
     console.log('I have been initialized')
     this.getHeroes()
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 import { HeroDetailModule } from '../hero-detail/hero-detail.module';
 import { HeroesComponent } from './heroes.component';
 
 
 @NgModule({
-  imports: [FormsModule, UpperCasePipe, CommonModule, HeroDetailModule],
+  imports: [FormsModule, UpperCasePipe, CommonModule, HeroDetailModule, AppRoutingModule],
   declarations: [
     HeroesComponent,
   ],
